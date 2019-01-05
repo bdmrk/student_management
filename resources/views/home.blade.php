f11d91da26d32f37405b0d9cbad002e0231761cf
Mahabubur Rahman Kausar <hostkausar@gmail.com>
2019-01-05 20:02:57 +0600

    new file added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    You are logged in! Hurrah...
                 </div>
             </div>
         </div>
