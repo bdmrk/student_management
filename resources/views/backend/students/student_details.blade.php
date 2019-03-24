8554870925c93df6390a9805a539d04d6b656b60
Mahabubur Rahman Kausar <hostkausar@gmail.com>
2019-03-24 11:12:24 +0600

    working on student sigle view

--- resources/views/backend/students/student_details.blade.php
+++ resources/views/backend/students/student_details.blade.php
@@ -121,39 +121,27 @@
 
                                                         <table class="table table-bordered"> <thead>
                                                             <tr>
-                                                                <th>#
-                                                                </th>
+                                                                <th>#</th>
+
                                                                 <th>Certificate</th>
                                                                 <th>Board/University</th>
-                                                                <th>Result</th> </tr>
                                                                 <th>Passing Year</th>
+                                                                <th>Result</th> </tr>
+
                                                             </tr>
                                                             </thead>
                                                             <tbody>
+                                                            @php($i=1)
+                                                            @foreach($student as $s)
                                                             <tr>
-                                                                <th scope="row">1</th>
-                                                                <td>Mark</td>
-                                                                <td>Otto</td>
-                                                                <td>@mdo</td>
+                                                                <th scope="row">{{$i++}}</th>
+                                                                <td>SSC</td>
+                                                                <td>Dhaka</td>
+                                                                <td>2005</td>
+                                                                <td>5.00</td>
                                                             </tr>
-                                                            <tr>
-                                                                <th scope="row">2</th>
-                                                                <td>Jacob</td>
-                                                                <td>Thornton</td>
-                                                                <td>@fat</td> </tr>
-                                                            <tr>
-                                                                <th scope="row">3</th>
-                                                                <td>Larry</td>
-                                                                <td>the Bird</td>
-                                                                <td>@twitter</td>
-                                                            </tr>
+                                                            @endforeach
 
-                                                            <tr>
-                                                                <th scope="row">3</th>
-                                                                <td>Larry</td>
-                                                                <td>the Bird</td>
-                                                                <td>@twitter</td>
-                                                            </tr>
                                                             </tbody>
                                                         </table>
                                                     </div>
